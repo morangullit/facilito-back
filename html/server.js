6c64aa0adc2e53146cd04712046bf9e8782aafca
gullit <gullitmoran@hotmail.com>
2023-11-21 10:40:42 -0500

    Motores de Vistas

--- html/server.js
+++ html/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 
 app.use('/assets', express.static('assets'));
+app.set('view engine', 'ejs');
 
 
 app.get('/', function(req, res){
-    res.sendFile('index.html', {
+    /* res.sendFile('index.html', {
         root: __dirname
-    });
+    }); */
+    res.render('index');
     //res.send(__dirname);
 });
 
